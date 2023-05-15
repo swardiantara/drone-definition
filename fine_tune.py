3e9b2177149db1140a082f140feda402cc21835d
Swardiantara Silalahi <35374847+swardiantara@users.noreply.github.com>
2023-05-15 13:13:51 +0700

    evaluation score for NER - done

--- fine_tune.py
+++ fine_tune.py
@@ -4,16 +4,9 @@ import math
 import numpy as np
 import pandas as pd
 import torch
-import argparse
-# from tqdm import tqdm, range
+from evaluation import build_pred_df, get_confusion_matrix, get_evaluation_score
 from simpletransformers.ner import NERModel, NERArgs
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-parser = argparse.ArgumentParser()
-
-# parser.add_argument('--model_name', type=str, default='bert-base-cased',
-#                     choices=['bert-base-cased', 'roberta-base', 'distilbert-base-cased', 'distilroberta-base', 'google/electra-base-discriminator', 'xlnet-base-cased'])
-parser.add_argument('--model_type', type=str, default='bert',
-                    choices=['bert', 'roberta', 'distilbert', 'distilroberta', 'electra', 'xlnet'], help='The model type to fine-tune')
 
 
 def load_dataset(path):
@@ -44,7 +37,7 @@ def get_model_args(model_type):
     args = NERArgs()
     model_type, model_name = get_model_name(model_type)
     model_name = model_name.split('/')[-1]
-    args.num_train_epochs = 1
+    args.num_train_epochs = 3
     args.learning_rate = 0.0001  # 0.0001  5e-5, 3e-5, 2e-5
     args.overwrite_output_dir = True
     args.train_batch_size = 16
@@ -57,7 +50,7 @@ def get_model_args(model_type):
 
 
 def check_test_data(test_df, preds_list):
-    dataset_test_group = test_df.groupby(['sentence_id'], as_index=False)[
+    dataset_test_group = test_df.groupby('sentence_id', group_keys=False, as_index=False)[
         'words', 'labels'].agg(lambda x: list(x))
     y_test = dataset_test_group['labels']
     compatible = True if len(preds_list) == len(y_test) else False
@@ -65,71 +58,47 @@ def check_test_data(test_df, preds_list):
     return compatible
 
 
-def build_pred_df(test_df, preds_list, strict=True):
-    dataset_test_group = test_df.groupby(['sentence_id'], as_index=False)[
-        'words', 'labels'].agg(lambda x: list(x))
-    y_test = dataset_test_group['labels']
-    x_test = dataset_test_group['words']
-
-    x_test_list = []
-    y_pred_list = []
-    y_test_list = []
-    for row in range(0, len(preds_list)):
-        x_test_list = np.concatenate((x_test_list, x_test[row]), axis=0)
-        y_pred_list = np.concatenate((y_pred_list, preds_list[row]), axis=0)
-        y_test_list = np.concatenate((y_test_list, y_test[row]), axis=0)
-    data_prediction = pd.DataFrame(
-        {'words': x_test_list, 'actual_class': y_test_list, 'predicted_class': y_pred_list})
-    confusion_matrix = pd.crosstab(
-        data_prediction['predicted_class'], data_prediction['actual_class'])
-    if strict == True:
-        data_prediction['strict'] = data_prediction.apply(
-            lambda row: True if row.actual_class == row.predicted_class else False, axis=1)
-    else:
-        data_prediction['predicted_non'] = data_prediction.apply(
-            lambda row: row.predicted_class.split('-')[-1], axis=1)
-        data_prediction['actual_non'] = data_prediction.apply(
-            lambda row: row.actual_class.split('-')[-1], axis=1)
-        data_prediction['non_strict'] = data_prediction.apply(lambda row: True if row.actual_class.split(
-            '-')[-1] == row.predicted_class.split('-')[-1] else False, axis=1)
-        confusion_matrix = pd.crosstab(
-            data_prediction['predicted_non'], data_prediction['actual_non'])
-    return data_prediction, confusion_matrix
-
-
 def main():
-    args = parser.parse_args()
-    device = True if torch.cuda.is_available() else False
-    model_type, model_name = get_model_name(args.model_type)
-    train_df, test_df = load_dataset('dataset')
+    model_types = ["bert", 'distilbert', 'roberta',
+                   'distilroberta', 'electra', 'xlnet']
 
-    train_stat = pd.Series(train_df["labels"].value_counts()).to_frame()
-    test_stat = pd.Series(test_df["labels"].value_counts()).to_frame()
+    for model_type in model_types:
+        device = True if torch.cuda.is_available() else False
+        model_type, model_name = get_model_name(model_type)
+        train_df, test_df = load_dataset('dataset')
 
-    # Save the train and test statistics into files
-    train_stat.to_csv('dataset/train_stat.csv')
-    test_stat.to_csv('dataset/test_stat.csv')
+        train_stat = pd.Series(train_df["labels"].value_counts()).to_frame()
+        test_stat = pd.Series(test_df["labels"].value_counts()).to_frame()
 
-    # Create a ClassificationModel
-    label = train_df["labels"].unique().tolist()
-    model_args = get_model_args(args.model_type)
-    model = NERModel(model_type, model_name, labels=label,
-                     args=model_args, use_cuda=device)
-    output_dir = getattr(model_args, "output_dir")
+        # Save the train and test statistics into files
+        train_stat.to_csv('dataset/train_stat.csv')
+        test_stat.to_csv('dataset/test_stat.csv')
 
-    # Fine-tune the model using our own dataset
-    model.train_model(train_df, eval_data=test_df, acc=accuracy_score)
+        # Create a ClassificationModel
+        label = train_df["labels"].unique().tolist()
+        model_args = get_model_args(model_type)
+        model = NERModel(model_type, model_name, labels=label,
+                         args=model_args, use_cuda=device)
+        output_dir = getattr(model_args, "output_dir")
 
-    # Evaluate the model
-    result, model_outputs, preds_list = model.eval_model(test_df)
-    compatible = check_test_data(test_df, preds_list)
-    if not compatible:
-        print('The length of test data and preds data is not compatible')
+        # Fine-tune the model using our own dataset
+        model.train_model(train_df, eval_data=test_df, acc=accuracy_score)
 
-    preds_df, confusion_matrix = build_pred_df(test_df, preds_list, True)
-    preds_df.to_csv("{}/prediction_{}.csv".format(output_dir, model_name))
-    confusion_matrix.to_csv(
-        "{}/confusion_matrix_{}.csv".format(output_dir, model_name))
+        # Evaluate the model
+        result, model_outputs, preds_list = model.eval_model(test_df)
+        compatible = check_test_data(test_df, preds_list)
+        if not compatible:
+            print('The length of test data and preds data is not compatible')
+
+        # Save the evaluation score to .csv files for error analysis
+        preds_df, cm = build_pred_df(test_df, preds_list, True)
+        preds_df.to_csv("{}/prediction_{}.csv".format(output_dir, model_name))
+        confusion_matrix = get_confusion_matrix(cm)
+        confusion_matrix.to_csv(
+            "{}/confusion_matrix_{}.csv".format(output_dir, model_name))
+        eval_dict, eval_df = get_evaluation_score(confusion_matrix, 'micro')
+        eval_df.to_csv(
+            "{}/confusion_matrix_{}.csv".format(output_dir, model_name))
 
 
 if __name__ == "__main__":
