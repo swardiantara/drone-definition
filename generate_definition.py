b8722daa60a083ff261956824bb00136f33a3ef3
Swardiantara Silalahi <swardyantara@gmail.com>
2023-06-09 14:39:15 +0700

    definition generation pipelines

--- generate_definition.py
+++ generate_definition.py
@@ -1,11 +1,16 @@
 import os
 import openai
+import requests
+import anthropic
+import json
 import pandas as pd
 import numpy as np
 import sqlite3
 from dotenv import load_dotenv
 from setup_db import get_connection
-from database.queries import get_term, insert_term, insert_definition
+from database.queries import get_term, insert_term, insert_definition, get_counter, get_definition
+from hugchat import hugchat
+from hugchat.login import Login
 
 load_dotenv()
 
@@ -15,30 +20,31 @@ def build_item_list(preds_df):
     term = ""
     label = ""
     prev_type = ""
+    index_verdict = preds_df.shape[1] - 1
     for row in range(0, preds_df.shape[0]):
         current_word = preds_df.iloc[row, 0]
         # next_word = preds_df.iloc[row + 1, 0]
         current_label = preds_df.iloc[row, 2]
         current_type = current_label.split('-')[-1]
-        # next_label = preds_df.iloc[row + 1, 2]
+        verdict = preds_df.iloc[row, index_verdict].lower()
         if current_label == 'O':
             if term != "":
-                print("index: ", row+1)
-                item_list.append((term, label))
+                # print("index: ", row+1)
+                item_list.append((term, label, verdict))
             term = ""
             label = ""
             continue
         else:
             if current_label.split('-')[0] == "B":
                 if term != "":
-                    print("index: ", row+1)
-                    item_list.append((term, label))
+                    # print("index: ", row+1)
+                    item_list.append((term, label, verdict))
                 term = current_word
                 label = current_label.split('-')[-1]
             elif prev_type != current_type:
                 if term != "":
-                    print("index: ", row+1)
-                    item_list.append((term, label))
+                    # print("index: ", row+1)
+                    item_list.append((term, label, verdict))
                 term = current_word
                 label = current_label.split('-')[-1]
             else:
@@ -54,19 +60,18 @@ def chatgpt_pipeline(db_path, item_list):
     openai.api_key = os.getenv("CHATGPT_KEY")
     # try:
     connection = get_connection(db_path)
-
-    for term, entity_type in item_list:
+    for term, entity_type, verdict in item_list:
         # Check if the term exists in the DB, insert if does not exists
         entity_type = entity_type.lower()
         term_exists = get_term(connection, term, entity_type)
         if term_exists == None:
-            insert_term(connection, term, entity_type)
+            insert_term(connection, term, entity_type, verdict)
 
         # Prompt if the definition does not exist
         prompt = "Provide the definition of {} drone {} from a drone expert perspective".format(
             term, entity_type)
         response = openai.Completion.create(
-            model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=1000)
+            model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=1000)
         definition = response.choices[0].text.strip()
 
         # Save the definition into .txt files for backup
@@ -82,10 +87,218 @@ def chatgpt_pipeline(db_path, item_list):
             connection, term, entity_type, 'chatgpt', prompt, definition)
         if isinstance(inserted_definition, sqlite3.Error):
             continue
+        elif inserted_definition == False:
+            continue
     print("\nChatGPT Pipeline finished successfully!")
     # except sqlite3.Error as error:
     #     connection.rollback()
     #     print("\nChatGPT Pipeline failed: ", error)
+
+
+def chatsonic_pipeline(db_path, item_list):
+    print("Running ChatSonic pipeline...")
+
+    apikey = os.getenv("CHATSONIC_KEY")
+
+    url = "https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=en"
+
+    connection = get_connection(db_path)
+    for term, entity_type, verdict in item_list:
+        # Check if the term exists in the DB, insert if does not exists
+        entity_type = entity_type.lower()
+        term_exists = get_term(connection, term, entity_type)
+        inserted_term = None
+        if term_exists == None:
+            inserted_term = insert_term(connection, term, entity_type, verdict)
+        term_id = term_exists[0] if term_exists != None else inserted_term
+        # Check if the definition exists
+        definition_exist = get_definition(connection,
+                                          term_id, 'chatsonic')
+        if definition_exist != None:
+            continue
+
+        # Prompt if the definition does not exist
+        prompt = "Provide the definition of {} drone {} from a drone expert perspective!".format(
+            term, entity_type)
+
+        payload = {
+            "enable_google_results": False,
+            "enable_memory": False,
+            "input_text": prompt
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-KEY": apikey
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        response_json = response.json()
+        definition = response_json['message']
+
+        # Save the definition into .txt files for backup
+        isExist = os.path.exists('definitions/chatsonic')
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs('definitions/chatsonic')
+        with open('definitions/chatsonic/{}-{}.txt'.format(term.replace(' ', '_'), entity_type), 'w') as file:
+            file.write(definition)
+
+        # Store the definition to the DB
+        inserted_definition = insert_definition(
+            connection, term, entity_type, 'chatsonic', prompt, definition)
+        if isinstance(inserted_definition, sqlite3.Error):
+            continue
+        elif inserted_definition == False:
+            continue
+    print("\nChatSonic Pipeline finished successfully!")
+
+
+def claude_pipeline(db_path, item_list):
+    print("Running Claude pipeline...")
+
+    apikey = os.getenv("CLAUDE_KEY")
+    client = anthropic.Client(apikey)
+
+    connection = get_connection(db_path)
+    for term, entity_type, verdict in item_list:
+        # Check if the term exists in the DB, insert if does not exists
+        entity_type = entity_type.lower()
+        term_exists = get_term(connection, term, entity_type)
+        if term_exists == None:
+            insert_term(connection, term, entity_type, verdict)
+
+        # Prompt if the definition does not exist
+        prompt = "Provide the definition of {} drone {} from a drone expert perspective!".format(
+            term, entity_type)
+
+        response = client.completion(
+            prompt=f"{anthropic.HUMAN_PROMPT} {prompt}{anthropic.AI_PROMPT}",
+            stop_sequences=[anthropic.HUMAN_PROMPT],
+            model="claude-v1",
+            max_tokens_to_sample=500,
+        )
+
+        definition = response['completion']
+        # Save the definition into .txt files for backup
+        isExist = os.path.exists('definitions/claude')
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs('definitions/claude')
+        with open('definitions/claude/{}-{}.txt'.format(term.replace(' ', '_'), entity_type), 'w') as file:
+            file.write(definition)
+
+        # Store the definition to the DB
+        inserted_definition = insert_definition(
+            connection, term, entity_type, 'claude', prompt, definition)
+        if isinstance(inserted_definition, sqlite3.Error):
+            continue
+        elif inserted_definition == False:
+            continue
+    print("\nClaude Pipeline finished successfully!")
+
+
+def huggingface_pipeline(db_path, item_list):
+    print("Running Huggingface pipeline...")
+
+    email = os.getenv("HF_EMAIL")
+    passwd = os.getenv("HF_PASSWORD")
+    # Log in to huggingface and grant authorization to huggingchat
+    sign = Login(email, passwd)
+    cookies = sign.login()
+
+    # Save cookies to usercookies/<email>.json
+    sign.saveCookies()
+
+    connection = get_connection(db_path)
+    for term, entity_type, verdict in item_list:
+        # Check if the term exists in the DB, insert if does not exists
+        entity_type = entity_type.lower()
+        term_exists = get_term(connection, term, entity_type)
+        if term_exists == None:
+            insert_term(connection, term, entity_type, verdict)
+
+        # Prompt if the definition does not exist
+        prompt = "Provide the definition of {} drone {} from a drone expert perspective!".format(
+            term, entity_type)
+
+        # Create a ChatBot
+        # or cookie_path="usercookies/<email>.json"
+        chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+        definition = chatbot.chat(prompt)
+
+        # Save the definition into .txt files for backup
+        isExist = os.path.exists('definitions/huggingchat')
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs('definitions/huggingchat')
+        with open('definitions/huggingchat/{}-{}.txt'.format(term.replace(' ', '_'), entity_type), 'w') as file:
+            file.write(definition)
+
+        # Store the definition to the DB
+        inserted_definition = insert_definition(
+            connection, term, entity_type, 'huggingchat', prompt, definition)
+        if isinstance(inserted_definition, sqlite3.Error):
+            continue
+        elif inserted_definition == False:
+            continue
+    print("\nHuggingface Pipeline finished successfully!")
+
+
+def alpaca_pipeline(db_path, item_list):
+    print("Running Alpaca pipeline...")
+
+    connection = get_connection(db_path)
+    for term, entity_type, verdict in item_list:
+        # Check if the term exists in the DB, insert if does not exists
+        entity_type = entity_type.lower()
+        term_exists = get_term(connection, term, entity_type)
+        inserted_term = None
+        if term_exists == None:
+            inserted_term = insert_term(connection, term, entity_type, verdict)
+        term_id = term_exists[0] if term_exists != None else inserted_term
+        # Check if the definition exists
+        definition_exist = get_definition(connection,
+                                          term_id, 'alpaca')
+        if definition_exist != None:
+            continue
+
+        # Prompt if the definition does not exist
+        prompt = "Provide the definition of {} drone {} from a drone expert perspective!".format(
+            term, entity_type)
+
+        response = requests.post("https://tloen-alpaca-lora.hf.space/run/predict", json={
+            "data": [
+                "Provide the definition of drone technical terms from a drone expert perspective!",
+                prompt,
+                0.1,
+                0.75,
+                40,
+                4,
+                500,
+            ]
+        }).json()
+        print(response)
+
+        definition = response['data'][0]
+
+        # Save the definition into .txt files for backup
+        isExist = os.path.exists('definitions/alpaca')
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs('definitions/alpaca')
+        with open('definitions/alpaca/{}-{}.txt'.format(term.replace(' ', '_'), entity_type), 'w') as file:
+            file.write(definition)
+
+        # Store the definition to the DB
+        inserted_definition = insert_definition(
+            connection, term, entity_type, 'alpaca', prompt, definition)
+        if isinstance(inserted_definition, sqlite3.Error):
+            continue
+        elif inserted_definition == False:
+            continue
+    print("\nAlpaca Pipeline finished successfully!")
 
 
 def main():
@@ -97,7 +310,10 @@ def main():
     item_list = build_item_list(pred_df)
     db_path = 'database/drone_definitions.db'
     # Run ChatGPT Pipeline to generate definitions from OpenAI ChatGPT
-    chatgpt_pipeline(db_path, item_list)
+    # chatgpt_pipeline(db_path, item_list)
+    # chatsonic_pipeline(db_path, item_list)
+    alpaca_pipeline(db_path, item_list)
+    # huggingface_pipeline(db_path, item_list)
     # terms = [('Obstacle Avoidance', 'function'), ('Palm Control',
     #   'function'), ('Auxiliary Bottom Light', 'component')]
     # for term, entity_type in item_list:
