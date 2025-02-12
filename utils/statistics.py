85f5d2437f25a3952dd767eb50bd4f227a4fd025
swardiantara <swardyantara@gmail.com>
2025-02-12 16:22:04 +0700

    include syntax statistics

--- utils/statistics.py
+++ utils/statistics.py
@@ -1,6 +1,6 @@
 # import sqlite3
 # from database.queries import get_all_definitions, insert_statistic, get_definition_by_id
-
+import statistics
 
 def count_words(definition):
     words = definition.split(' ')
@@ -29,3 +29,26 @@ def compute_statistic(definition):
     paragraph = count_paragraph(definition)
 
     return unique, words, sentence, paragraph
+
+
+def compute_mean_statistics(definitions):
+    unq = []
+    wrd = []
+    snt = []
+    prg = []
+
+    for definition in definitions:
+        unique, words, sentence, paragraph = compute_statistic(definition)
+        unq.append(unique)
+        wrd.append(words)
+        snt.append(sentence)
+        prg.append(paragraph)
+
+    statistic = {
+        'unq': round(statistics.mean(unq), 3),
+        'wrd': round(statistics.mean(wrd), 3),
+        'snt': round(statistics.mean(snt), 3),
+        'prg': round(statistics.mean(prg), 3),
+    }
+
+    return statistic
